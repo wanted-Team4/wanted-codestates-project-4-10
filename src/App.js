1bd379993f33be824dd87049aba9aca01e546266
slobbie <haeru9410@gmail.com>
2022-03-11 00:48:14 +0900

    :bug:  fix 사용되지 않는 패키지로 인한 오류 수정

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/configure';
-import { persistStore } from 'redux-persist';
-import { PersistGate } from 'redux-persist/integration/react';
 import Home from './Page/Home';
-
-const persistor = persistStore(store);
 
 function App() {
   return (
     <BrowserRouter>
       <Provider store={store}>
-        <PersistGate persistor={persistor}>
-          <Home />
-        </PersistGate>
+        <Home />
       </Provider>
     </BrowserRouter>
   );
